48249055ad339dce046adae09fd96ca827ca0aef
nadeengogary <nadeengogary@gmail.com>
2021-07-12 03:33:53 +0200

    debugging

--- bin/iVectors_gmm/gmm.py
+++ bin/iVectors_gmm/gmm.py
@@ -102,9 +102,11 @@ def main(mode):
 				X_train_old = read_train(m, mid, mode)
 				X_train_dim = X_train_old.ndim
 				X_train =get_model(X_train_dim)
-				X_test_old, eval_files_old = read_test(m, mid, mode)
+				X_test_old, Y_test_old = read_test(m, mid, mode)
 				X_test_dim = X_test_old.ndim
-				X_test,eval_files = get_model(X_test_dim)
+				Y_test_dim = Y_test_old.ndim
+				X_test = get_model(X_test_dim)
+				Y_test = get_model(X_test_dim)
 				y_pred_iv = GMM(X_train, X_test)
 
 				AUC = roc_auc_score(y_test, y_pred_iv)
@@ -149,7 +151,8 @@ def main(mode):
 				X_train = get_model(X_train_dim)
 				X_test_old, eval_files_old = read_test(m, mid, mode)
 				X_test_dim = X_test_old.ndim
-				X_test,eval_files = get_model(X_test_dim)
+				X_test = get_model(X_test_dim)
+				eval_files = get_model(X_test_dim)
 
 				y_pred_iv = GMM(X_train, X_test)
 				anom_scores['file'] = eval_files
