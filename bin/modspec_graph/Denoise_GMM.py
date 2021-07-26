96d1ff1339aa2ab367296539880bab89e923bc91
nadeengogary <nadeengogary@gmail.com>
2021-07-27 01:50:07 +0200

    debugging

--- bin/modspec_graph/Denoise_GMM.py
+++ bin/modspec_graph/Denoise_GMM.py
@@ -9,7 +9,7 @@ from keras import optimizers
 
 def TRAIN_DENOISE(X):
     # X = X.reshape(-1)
-    n_input_dim = X.shape[1]
+    n_input_dim = X.shape[2]
     # n_output_dim = Y.shape[1]
 
     n_hidden1 = 2049
