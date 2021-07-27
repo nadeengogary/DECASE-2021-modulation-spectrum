8b61e37aeb926db1d5f43791765cbae38ee330bf
nadeengogary <nadeengogary@gmail.com>
2021-07-27 02:15:21 +0200

    debugging

--- bin/modspec_graph/Denoise_GMM.py
+++ bin/modspec_graph/Denoise_GMM.py
@@ -49,7 +49,7 @@ def TRAIN_DENOISE(X):
     tensorboard = TensorBoard(log_dir="./logs", histogram_freq=0, write_graph=True, write_images=True)
     # fit the model
 
-    # X = X.reshape(len(X),-1)
+    X = X.reshape(len(X),-1)
     # X = X.shape[0:1]
     # print(X.shape[0])
     # hist = model.fit(X
