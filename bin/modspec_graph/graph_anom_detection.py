85b91aba071c64c60bd655788d2ec3e3994b9db1
nadeengogary <nadeengogary@gmail.com>
2021-08-01 22:29:12 +0200

    debugg

--- bin/modspec_graph/graph_anom_detection.py
+++ bin/modspec_graph/graph_anom_detection.py
@@ -80,8 +80,8 @@ def main(mode):
 
                 X_train_old, X_test, y_test = get_spectrums(
                     m, mid, reducenoise, mode)
-                # X_train = TRAIN_DENOISE(np.array(X_train),np.array(X_test))
-                X_train = get_model(np.array(X_train_old),np.array(X_test))
+                X_train = TRAIN_DENOISE(np.array(X_train),np.array(X_test))
+                # X_train = get_model(np.array(X_train_old),np.array(X_test))
 
                 subgraphs, Graph = build_net(X_train)
                 means, deviations = get_means(X_train_old, subgraphs)
