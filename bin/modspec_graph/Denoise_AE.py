45536cae2da50d675a2e3bb41ec474631a678a84
nadeengogary <nadeengogary@gmail.com>
2021-08-01 22:42:38 +0200

    debugg

--- bin/modspec_graph/Denoise_AE.py
+++ bin/modspec_graph/Denoise_AE.py
@@ -51,7 +51,7 @@ def TRAIN_DENOISE(X,X_test):
 
     tensorboard = TensorBoard(log_dir="./logs", histogram_freq=0, write_graph=True, write_images=True)
     # fit the model
-    # print(X.shape)
+    print(X.shape)
     # X = X.T
     X = X.reshape(len(X),-1)
     print(X.shape)
